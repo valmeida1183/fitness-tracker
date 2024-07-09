379857733182b8a575851b503bb882975b56715f
Vinicius Lopes de Almeida <termobin@gmail.com>
2024-07-08 21:30:44 -0300

    Finished training page

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -6,9 +6,13 @@ import {
   OnInit,
   viewChild,
 } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { DatePipe } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 import { Exercise } from './models/exercise.model';
 import { TrainingService } from '../services/training.service';
@@ -16,7 +20,15 @@ import { TrainingService } from '../services/training.service';
 @Component({
   selector: 'app-past-training',
   standalone: true,
-  imports: [MatTableModule, MatSortModule, DatePipe],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    FlexLayoutModule,
+    DatePipe,
+  ],
   templateUrl: './past-training.component.html',
   styleUrl: './past-training.component.scss',
 })
@@ -30,13 +42,15 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   ];
   dataSource = new MatTableDataSource<Exercise>();
   sort = viewChild<MatSort>(MatSort);
+  paginator = viewChild<MatPaginator>(MatPaginator);
 
   private trainingService = inject(TrainingService);
 
   constructor() {
-    // Se usar effect ou ngAfterViewInit é tem o mesmo resultado.
+    // Se usar effect ou ngAfterViewInit é o mesmo resultado.
     effect(() => {
       this.dataSource.sort = this.sort() ?? null;
+      this.dataSource.paginator = this.paginator() ?? null;
     });
   }
 
@@ -47,5 +61,11 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     //this.dataSource.sort = this.sort() ?? null;
+    //this.dataSource.paginator = this.paginator() ?? null;
+  }
+
+  onFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
